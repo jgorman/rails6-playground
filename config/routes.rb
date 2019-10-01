38d2345f6beb1e22e2ddfeff4cae714f0577bc47
John Gorman <johngorman2@gmail.com>
2019-10-01 15:11:38 -0300

    jQuery running.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get 'welcome/index'
+  get "welcome/rails6-config"
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    // Popper: ['popper.js', 'default']
+  })
+)
+
+const aliasConfig = {
+    'jquery': 'jquery/src/jquery',
+    // 'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+
+};
+environment.config.set('resolve.alias', aliasConfig);
+
+// const BundleAnalyzer = require('webpack-bundle-analyzer')
+// const BundleAnalyzerPlugin = BundleAnalyzer.BundleAnalyzerPlugin
+// environment.plugins.prepend('Analyzer', BundleAnalyzerPlugin)
+
 module.exports = environment
