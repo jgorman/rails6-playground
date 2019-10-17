085a53d6fa39946efc77c37e8951f65cd31d751f
John Gorman <johngorman2@gmail.com>
2019-10-17 16:55:26 -0300

    Datatables stateSave enabled.

--- app/javascript/web/controllers/articles_datatable_controller.js
+++ app/javascript/web/controllers/articles_datatable_controller.js
@@ -1,24 +1,35 @@
 import DataTable from 'stimulus-datatables'
 
 export default class extends DataTable {
+
+  initialize() {
+    if (!this.isBooting()) return
+
+    // Link to the scrolling checkbox and back.
+    const checkbox = $('#toggle-scrolling')[0]
+    if (checkbox) {
+      checkbox.dt = this
+      this.checkbox = checkbox
+    }
+
+    // Get the table config and the scrolling state.
+    super.initialize()
+    this.setScrollingState()
+  }
+
   toggle_scrolling = event => {
-    const table = $('#articles-datatable')[0]
-    if (table && table.dt) {
-      const dt = table.dt
+    const dt = this.element.dt
+    if (dt) {
       dt.setScrollingState()
       dt.teardown() // This triggers a reconnect.
     }
   }
 
-  initialize() {
-    super.initialize() && this.setScrollingState()
-  }
-
   setScrollingState = () => {
-    const checkbox = $('#toggle-scrolling')[0]
-    const scrolling = checkbox && checkbox.checked
+    const scrolling = this.checkbox && this.checkbox.checked
     const config = this.config || {}
     config.scroller = scrolling
     config.scrollY = scrolling ? 600 : undefined
+    config.stateSave = !scrolling
   }
 }
